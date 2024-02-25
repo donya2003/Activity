79f2893a7c8ca55f7fd078a000fcf40291d0c5e4
donya2003 <152689935+donya2003@users.noreply.github.com>
2024-02-25 21:48:46 +0700

    Add files via upload

---
+++ time.js
@@ -0,0 +1,10 @@
+function updateDateTime(){
+    var datetimeElement = document.getElementById('datetime');
+    if(datetimeElement){
+        var now = new Date();
+        var dateTimeString = now.toLocaleString();
+        datetimeElement.textContent = "" + dateTimeString;
+    }
+}
+updateDateTime();
+setInterval(updateDateTime, 1000);
